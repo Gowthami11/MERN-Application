bebefa9cc45bb8d992704f7b2887bdc56566441a
Gowthami11 <gowthami.pediredla@yahoo.com>
2022-01-22 00:13:28 +0530

    image path changed

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use((error, req, res, next) => {
     if (res.headerSent) {
         next(error)
     }
-    res.status(error.code || 500);
+    // res.status(error.code || 500);
+    res.status(typeof error.code === 'number' ? error.code : 500)
     res.json({ message: error.message || 'An Unknown error has occured' })
 })
 mongoose.connect(dburl).then(() => app.listen(process.env.PORT||5000, () => console.log('connection successfull')))
